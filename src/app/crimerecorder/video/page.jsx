80c4e4a87b70ee7a2c8b18abad1042cf84dd283c
Jerydam <jerydan148@gmail.com>
2024-07-01 01:46:06 +0100

    on video recording

--- src/app/crimerecorder/video/page.jsx
+++ src/app/crimerecorder/video/page.jsx
@@ -3,8 +3,27 @@ import React from "react";
 import icon4 from "../../../../public/pause.png";
 import { Recording } from "../components/Recording";
 import { Header } from "../components/Header";
+import { client } from "@/utils/thirdwebclient";
+import { useWriteToContract } from "@/utils/fetchcontract";
 
-const VideoRecorder = ({ uri }) => {
+const VideoRecorder = () => {
+  const {
+    sendTransaction,
+    transaction,
+    isPending,
+    isLoading,
+    error,
+    data,
+    isSuccess,
+  } = useWriteToContract(client, "crime", "function coverCrime(string uri)", [
+    "bafybeigw4itap22hfpruchbtzmveyykeeaio3vw5ypjflgjyajf73h5qqe",
+  ]);
+
+  const handleSubmit = () => {
+    sendTransaction(transaction);
+    console.log("after send tx", isSuccess && transaction);
+  };
+
   const text = {
     text3: `Record a video to keep on the blockchain`,
   };
@@ -12,13 +31,8 @@ const VideoRecorder = ({ uri }) => {
   return (
     <div className="h-screen w-full m-10">
       <Header />
-      <div className="flex justify-center">
-        <Recording
-          text={text.text3}
-          icon1={icon4}
-          imgText={`Stop Recording`}
-          uri={uri}
-        />
+      <div onClick={handleSubmit} className="flex justify-center">
+        <Recording text={text.text3} icon1={icon4} imgText={`Stop Recording`} />
       </div>
     </div>
   );
