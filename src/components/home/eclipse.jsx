d6788b0e45a437fb860a7fdcb581073a623e4fc4
goodness5 <goodnesskolapo@gmail.com>
2024-08-16 03:38:35 +0100

    fixed navbar dropdowns and added some animations

---
+++ src/components/home/eclipse.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Shape = () => {
+  return (
+    <div className="w-[30em] h-[14em] mb-16 mx-auto">
+      <div className="shape"></div>
+    </div>
+  );
+};
+
+export default Shape;
