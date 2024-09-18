d402b348e8cdaef1de9e184b0320589825832446
goodness5 <goodnesskolapo@gmail.com>
2024-09-18 10:00:58 +0100

    build_error_fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 "use client";
+import { usePathname } from "next/navigation";
 import "./globals.css";
 import Footer from "@/components/footer";
 import Metadata from "./metadata";
@@ -6,18 +7,36 @@ import BackgroundWrapper from "@/components/backgroundwrapper";
 import { WalletProvider } from "@/components/walletprovider";
 
 export default function RootLayout({ children }) {
+  const pathname = usePathname();
+
+  if (pathname === "/crimerecorder") {
+    return (
+      <html lang="en">
+        <head>
+          <title>404 Not Found</title>
+        </head>
+        <body className="flex flex-col min-h-screen justify-between">
+          <BackgroundWrapper>
+            <div className="w-full flex flex-col justify-center items-center min-h-screen">
+              <h1 className="text-4xl">404 - Page Not Found</h1>
+            </div>
+          </BackgroundWrapper>
+        </body>
+      </html>
+    );
+  }
+
   return (
     <html lang="en">
       <head>
         <title>{Metadata.title}</title>
         {Metadata.link}
-    
       </head>
       <body className="flex flex-col min-h-screen justify-between">
         <BackgroundWrapper>
           <WalletProvider>
             <div className="w-full flex flex-col justify-between">
-              <div className="min-h-screen w-full ">{children}</div>
+              <div className="min-h-screen w-full">{children}</div>
               <div className="h-fit">
                 <Footer />
               </div>
