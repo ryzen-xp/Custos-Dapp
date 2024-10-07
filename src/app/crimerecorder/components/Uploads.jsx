74446aec88a63e3ae46efdc9be0d76c22151714a
jerydam <jerydan148@gmail.com>
2024-10-07 21:40:39 +0100

    crimerecorder done

--- src/app/crimerecorder/components/Uploads.jsx
+++ src/app/crimerecorder/components/Uploads.jsx
@@ -53,7 +53,6 @@ import React, { useContext, useEffect, useState } from "react";
 import { WalletContext } from "@/components/walletprovider"; 
 import NoRecordScreen from "./NoRecordScreen";
 
-
 const Uploads = () => {
   const { account } = useContext(WalletContext); // Using WalletContext to get connected account
   const [uploadedFiles, setUploadedFiles] = useState([]);
@@ -88,30 +87,30 @@ const Uploads = () => {
         {!uploadedFiles.length ? (
           <NoRecordScreen/>
         ) : (
-          <div className="grid grid-cols-3 gap-4 text-white">
+          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 text-white">
             {uploadedFiles.map((file, index) => (
-              <div key={index} className=" text-[2em] whitespace-nowrap mb-2 sm:mb-0 bg-gradient-to-r from-[#0094FF] to-[#A02294] bg-clip-text text-transparent ">
+             <div key={index} className=" text-[2em] whitespace-nowrap mb-2 sm:mb-0 bg-gradient-to-r from-[#0094FF] to-[#A02294] bg-clip-text text-transparent ">
+               
                 {/* Display image or video based on file type */}
                 {isImageFile(file.fileName) ? (
                   <img
                     src={`https://gateway.pinata.cloud/ipfs/${file.ipfsHash}`}
                     alt={file.fileName}
-                    className="w-full h-auto mt-2"
+                    className="w-full h-auto rounded-md"
                   />
                 ) : isVideoFile(file.fileName) ? (
                   <video
                     src={`https://gateway.pinata.cloud/ipfs/${file.ipfsHash}`}
                     controls
-                    className="w-full h-auto mt-2"
+                    className="w-full h-auto rounded-md"
                   />
                 ) : (
-                  <p className="text-red-500 mt-2">Unsupported file type</p>
+                  <p className="text-red-500">Unsupported file type</p>
                 )}
-                <h3 className="font-bold text-lg">{file.fileName}</h3>
-                <p className="text-sm text-gray-600">
+                <h3 className="font-bold text-lg mt-2">{file.fileName}</h3>
+                <p className="text-sm text-gray-400">
                   Uploaded on: {new Date(file.timestamp).toLocaleString()}
                 </p>
-                
               </div>
             ))}
           </div>
