89bc4f924bd1f4a964c8febccc033e5286e1a5c8
evgongora <evasquezgongora@gmail.com>
2024-10-28 13:40:24 -0600

    fix: added button functionality and corrections to UI

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,23 +1,39 @@
-import React from "react";
+"use client";
+import React, { useState, useRef } from "react";
 import Image from "next/image";
 import Navbar from "@/components/navbar";
-import Footer from "@/components/footer";
+import ShowLaunchDapps from "../../components/showLaunchDapps";
 
 const About = () => {
+  const [showLaunchDapps, setShowLaunchDapps] = useState(false);
+  const starRef = useRef(null);
+
+  const toggleLaunchDapps = () => {
+    setShowLaunchDapps(!showLaunchDapps);
+  };
+  const closeModal = () => {
+    setShowLaunchDapps(false);
+  };
+
   return (
     <main className="items-center justify-center min-h-screen">
       <Navbar />
       <div className="text-white py-20 mx-auto flex flex-col justify-center items-center w-full max-w-screen-xl px-4">
-        <a href="/services">
-          <button className="relative max-w-sm w-full text-white p-3 shadow-lg transform hover:scale-105 transition-transform duration-300 border-gradient bg-opacity-50 mt-20 mb-5 backdrop-filter backdrop-blur-lg flex items-center justify-center overflow-clip">
+        <div className="flex w-fit h-fit">
+          <button 
+            onClick={toggleLaunchDapps}
+            className="relative br w-full text-white shadow-lg py-3 px-6 transform hover:scale-105 transition-transform duration-300 border-gradient bg-opacity-50 mt-20 mb-5 backdrop-filter backdrop-blur-lg flex items-center justify-center"
+          >
             <span className="flex items-center">Launch Custos Dapp</span>
             <img
               src="/star.png"
-              className="absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-4 h-4 z-20"
+              className="absolute w-6 h-8 z-20 animate-star"
               alt="Star Icon"
             />
           </button>
-        </a>
+        </div>
+
+        {showLaunchDapps && <ShowLaunchDapps closeModal={closeModal} />}
 
         <p className="text-4xl font-bold mb-2 bg-gradient-to-r from-[#EAF9FF] to-[#8E9A9A] bg-clip-text text-transparent text-center w-full p-3">
           We are building a safe on the <br /> blockchain for your assets
@@ -36,60 +52,62 @@ const About = () => {
         </div>
       </div>
 
-      <div className="mx-5 transform hover:scale-105 transition-transform duration-300">
-        <div className="flex flex-col md:flex-row justify-between items-center my-32 ml-10">
-          <div className="mb-2 bg-gradient-to-r from-[#0094FF] to-[#A02294] bg-clip-text text-transparent md:text-[50px] text-[30px]">
-            <p className="mb-12 text-3xl">About Custos Diretriz</p> {/* Changed from text-4xl to text-3xl */}
+      <div className="container mx-auto transform hover:scale-105 transition-transform duration-300">
+        <div className="flex flex-col md:flex-row justify-between items-center my-32">
+          <div className="md:w-1/2 mb-2 bg-gradient-to-r from-[#0094FF] to-[#A02294] bg-clip-text text-transparent md:text-[50px] text-[30px] text-center md:text-center">
+            <p className="mb-12 text-3xl text center">About Custos Diretriz</p>
             <div className="flex justify-center">
               <Image
                 src="/ElliH.png"
                 alt="Image"
                 width={200} // Keep the width smaller for desktop
                 height={100} // Keep the height smaller for desktop
-                className="w-48 h-auto md:w-32 eclipse" // Added bounce class here
+                className="w-48 h-auto md:w-32 pt-5 eclipse"
               />
             </div>
           </div>
-          <div className="md:w-1/2 md:ml-4 text-[20px] bg-gradient-to-r from-[#EAF9FF] to-[#8E9A9A] bg-clip-text text-transparent">
+          <div className="md:w-1/2 max-w-sm mx-auto text-[20px] bg-gradient-to-r from-[#EAF9FF] to-[#8E9A9A] bg-clip-text text-transparent text-center md:text-left mt-10 md:mt-0">
             <p>
-              At Custos Diretriz, we are dedicated to revolutionizing safety{" "}
+              At Custos Diretriz, we are dedicated to revolutionizing safety and security{" "}
               <br />
-              and security through innovative protocol platforms.
+              through innovative protocol platforms.
             </p>
             <br />
             <p>
-              Our mission is to empower individuals and communities to <br />
-              effectively address crime scene witnessing and streamline <br />{" "}
-              agreement systems.
+              Our mission is to empower individuals and communities to crime scene witnessing<br />
+              and streamline agreement systems.
+              
             </p>
           </div>
         </div>
       </div>
 
-      <div className="flex mt-8 items-center justify-center bg-gradient-to-r from-[#EAF9FF] to-[#8E9A9A] bg-clip-text text-transparent">
+      <div className="flex mt-8 items-center justify-center transform hover:scale-105 transition-transform duration-300">
         <div className="text-center p-8 bg-transparent rounded shadow-lg">
-          <div className="flex flex-col md:flex-row items-center justify-center">
-            <p className="text-[1.5em] mb-4 md:mb-0 mr-2">Our vision</p>
-            <Image
-              src="/ElliS.png"
-              alt="Image"
-              width={200}
-              height={100}
-              className="w-12 h-auto md:w-32 md:mr-4 mb-4 md:mb-0 eclipse" // Adjust width for responsiveness
-            />
+          <div className="flex flex-col md:flex-row items-center justify-center bg-gradient-to-r from-[#0094FF] to-[#A02294] bg-clip-text text-transparent md:text-[50px] text-[30px]">
+            <p className="text-3xl mb-4 md:mb-0 mr-2">Our vision</p>
+            <div className="flex justify-center">
+              <Image
+                src="/ElliS.png"
+                alt="Image"
+                width={200}
+                height={100}
+                className="w-12 h-auto md:w-32 md:mr-4 mb-4 md:mb-0 pt-8 eclipse"
+              />
+            </div>
           </div>
 
-          <div className="text-left mt-4">
+          <div className="text-left mt-6">
             {" "}
             {/* Adds some margin above the text paragraphs */}
-            <div className="text-center"> {/* Added this div for center alignment */}
-              <p className="text-[20px] mb-4">
-                By leveraging technology and collaboration, we strive to build<br />
-                 a world where everyone feels empowered to make a <br />
+            <div className="mx-auto text-[20px] bg-gradient-to-r from-[#EAF9FF] to-[#8E9A9A] bg-clip-text text-transparent text-center">
+              <p>
+                By leveraging technology and collaboration, we strive to build a world<br />
+                where everyone feels empowered to make a <br />
                 difference in their communities.
               </p>
               <br />
-              <p className="text-[1em] mb-4">
+              <p>
                 We envision a safer and more transparent society where individuals<br />
                  have the tools and resources they need to contribute to <br />
                 crime prevention and intervention efforts.
